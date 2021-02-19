c62a4b3b9920d14d0e13b34735e6df69bd15b687
frankhsu <frank58200@hotmail.com>
2021-02-19 20:46:04 +0800

    Add ODF Read XML Console

---
+++ ODFConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+
+namespace ODFConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var file = ZipFile.Open("t_acc_1_o.odt", ZipArchiveMode.Read))
+            {
+                //To get the content of ODF
+                var result = file.GetEntry("content.xml");
+
+                //To check the content exist
+                if (result != null)
+                {
+                    XmlDocument document = new XmlDocument();
+                    //XmlReader reader = XmlReader.Create(result.Open());
+                    document.Load(result.Open());
+
+                    var resu = document.GetElementsByTagName("table:table-row");
+
+                    foreach (XmlNode row in resu)
+                    {
+                        var child = row.ChildNodes;
+
+                        foreach (XmlNode col in child)
+                        {
+                            Console.WriteLine(col.InnerText);
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+}
